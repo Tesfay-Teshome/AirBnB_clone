7ba6e0e6c1b67abda434e8cb882bfab9afe0a6c5
Tesfay-Teshome <tesfix2017@gmail.com>
2022-08-31 05:26:01 -0700

    City file with City class inherted from BaseModel

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+City Class that Inherited from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+
+    """ Class that defines attributes for a city """
+
+    state_id = ""
+    name = ""
