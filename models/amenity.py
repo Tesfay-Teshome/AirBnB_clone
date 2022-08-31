65ee2f526e3c9b92451683c9161399cb3fa62f30
Tesfay-Teshome <tesfix2017@gmail.com>
2022-08-31 04:46:32 -0700

    Amenity Class for console file

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" 
+Amenity Class that inherits from BaseModel
+"""
+
+from models.bash_model import BaseModel
+
+class Amenity(BaseModel):
+    """ Class that define attributes for amenity """
+
+    name = " "
